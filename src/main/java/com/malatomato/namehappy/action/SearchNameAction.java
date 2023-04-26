9f66101344b85c5a108116db9d46b99912d8a1a3
Willson Xiong <willson.xiong@wearecgl.com>
2023-04-26 13:58:13 +0800

    feat: init

---
+++ src/main/java/com/malatomato/namehappy/action/SearchNameAction.java
@@ -0,0 +1,20 @@
+package com.malatomato.namehappy.action;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * [nameHappy]
+ * [com.malatomato.namehappy.action]
+ * [2023/04/26 13:44]
+ *
+ * @author [willson.xiong]
+ * @version 1.0.0
+ */
+public class SearchNameAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
